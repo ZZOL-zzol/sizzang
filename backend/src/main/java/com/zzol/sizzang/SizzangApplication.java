1f9e35e0bcd82c9dd109736071a6634ea6fa58c4
KMS <sakmsik98@naver.com>
2023-09-02 16:22:48 +0900

    chore: BE 초기환경세팅

---
+++ backend/src/main/java/com/zzol/sizzang/SizzangApplication.java
@@ -0,0 +1,13 @@
+package com.zzol.sizzang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SizzangApplication.class, args);
+	}
+
+}
