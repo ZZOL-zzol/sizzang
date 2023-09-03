4af3492902b2e71f5fb56705e6ed8717243dab3f
Jaewoong Jeong <jjwoong1733@gmail.com>
2023-09-03 11:30:21 +0900

    :sparkles: feat: Header 추가

--- frontend/src/views/MainPage.js
+++ frontend/src/views/MainPage.js
@@ -1,10 +1,11 @@
+import Header from '../components/common/Header';
 import Navbar from '../components/common/Navbar';
 
 const MainPage = () => {
   return (
-    <div className="App text-3xl h-full">
-      
-      <div className='h-96 bg-gray-200'>바디이이이이</div>
+    <div className="App flex flex-col text-3xl h-full w-full">
+      <Header/>
+      <div className='h-96  flex flex-col flex-grow'>바디이이이이</div>
       <Navbar/>
     </div>
   );
