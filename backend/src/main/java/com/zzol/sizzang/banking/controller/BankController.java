fdd419a9c9e119242db30821e3d63154ae24f867
chaeyeong222 <iky1505@naver.com>
2023-09-09 16:36:08 +0900

    :sparkles: feat: 1원 이체 기능 구현 Won1TransferResponseDto, Response 추가 인증키+ZZOL 형식으로  본인 계좌 확인 키 설정

--- backend/src/main/java/com/zzol/sizzang/banking/controller/BankController.java
+++ backend/src/main/java/com/zzol/sizzang/banking/controller/BankController.java
@@ -1,21 +1,34 @@
 package com.zzol.sizzang.banking.controller;
 
 
+import com.zzol.sizzang.banking.dto.Request.Won1TransferRequestDto;
 import com.zzol.sizzang.banking.service.BankService;
+import com.zzol.sizzang.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/bank")
 @RestController
 @CrossOrigin
 public class BankController {
 
     private final BankService bankService;
+    private final UserService userService;
+
+    @Operation(description = "1원이체 메서드")
+    @PostMapping("/v1/auth/1transfer")
+    public ResponseEntity<?> won1Transfer(@RequestBody Won1TransferRequestDto won1TransferRequestDto) {
+        log.info("won1Transfer 요청");
+        String checkMemo = bankService.won1Transfer(won1TransferRequestDto);
+        return new ResponseEntity<>(checkMemo, HttpStatus.OK);
+    }
+
 
 
 
--- backend/src/main/java/com/zzol/sizzang/banking/service/BankService.java
+++ backend/src/main/java/com/zzol/sizzang/banking/service/BankService.java
@@ -1,4 +1,27 @@
 package com.zzol.sizzang.banking.service;
 
+import com.zzol.sizzang.banking.dto.Request.Won1TransferRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+//@Transactional
+@RequiredArgsConstructor
 public class BankService {
+
+
+
+
+    public String won1Transfer(Won1TransferRequestDto won1TransferRequestDto){
+        // TODO: 거래 내역 업데이트 기능 추가
+        String userAccount = won1TransferRequestDto.getAccount();
+        String userBankCode = won1TransferRequestDto.getBankCode();
+        String memo = won1TransferRequestDto.getMemo();
+        memo = memo.replace("ZZOL",""); //서비스명 제거
+        memo = memo.replace(" ",""); //공백제거
+        log.info("won1Transfer key: {}", memo);
+        return memo;
+    }
 }
