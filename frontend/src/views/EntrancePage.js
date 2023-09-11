af70504cd7bedb46fc1d702bcaaa4ecd2e9db59f
Jaewoong Jeong <jjwoong1733@gmail.com>
2023-09-11 23:20:10 +0900

    :sparkles: feat: 로그인, 회원가입 api 요청 제작 - 현재 cors 에러 발생, 수정 필요

--- frontend/src/views/EntrancePage.js
+++ frontend/src/views/EntrancePage.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/common/Button";
 import TextInput from "../components/common/TextInput";
 import axios from "axios";
-
-const LOGIN_URL = "http://localhost:8080/oauth/login/kakao";
+import { API_URL } from "../lib/constants";
 
 const EntrancePage = () => {
+  const navigate = useNavigate()
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const onUserIdChange = (e) => {
+    setUserId(e.target.value)
+  };
+
+  const onUserPasswordChange = (e) => {
+    setUserPassword(e.target.value)
+  }
+
+
   const onLoginClick = () => {
-    // axios
-    //   .post(
-    //     `${LOGIN_URL}`,
-    //     JSON.stringify({ userId: "userId", userPassword: "userPassword" }),
-    //     {
-    //       headers: { "Content-Type": "application/json" },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     if (res.data) {
-    //     }
-    //   });
+    axios
+      .get(
+        `${API_URL}/user/login`,
+        JSON.stringify({ userId: userId, userPassword: userPassword }),
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        if (res.data) {
+          window.localStorage.setItem('Access_Token',res.data.ownJwtAccessToken);
+          window.localStorage.setItem('Refresh_Token',res.data.ownJwtRefreshToken);
+          // window.localStorage.setItem()
+          
+        }
+      })
+      .then(
+        navigate('/main')
+      );
   };
 
   return (
@@ -31,17 +50,17 @@ const EntrancePage = () => {
       </div>
       <div className="flex flex-col h-2/5 justify-evenly  items-center">
         <div className="flex flex-col gap-2 w-full px-9">
-          <TextInput placeholder="ID" />
-          <TextInput placeholder="PASSWORD" />
+          <TextInput placeholder="ID" onChangeEvent={onUserIdChange} value={userId}/>
+          <TextInput placeholder="PASSWORD" onChangeEvent={onUserPasswordChange} value={userPassword}/>
         </div>
         <div className="flex flex-col gap-2 w-full items-center px-9">
-          <Link to={"/main"} className="w-full">
+          {/* <Link to={"/main"} className="w-full"> */}
             <Button
               innerText="로그인"
               color="bg-blue-200"
               onClick={() => onLoginClick}
             />
-          </Link>
+          {/* </Link> */}
           <Link to={"/signup"} className="w-full">
             <Button innerText="회원가입" color="bg-blue-500" />
           </Link>
