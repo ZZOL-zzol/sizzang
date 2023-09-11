660968c26c824e2b5f8a39276c46a48821d5ac61
chaeyeong222 <iky1505@naver.com>
2023-09-11 17:30:54 +0900

    :sparkles: feat: 1원이체시 거래내역 db와 계좌 db 동시에 데이터 업뎃 기능 구현

--- backend/src/main/java/com/zzol/sizzang/banking/entity/Bank.java
+++ backend/src/main/java/com/zzol/sizzang/banking/entity/Bank.java
@@ -1,12 +1,14 @@
 package com.zzol.sizzang.banking.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
 @ToString
 @Getter
 @Entity
