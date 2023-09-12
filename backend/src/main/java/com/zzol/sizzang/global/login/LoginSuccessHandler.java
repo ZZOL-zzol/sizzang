c57be6d1163deddd9760f8d92b3a803d4d1f7312
chaeyeong222 <iky1505@naver.com>
2023-09-12 20:18:57 +0900

    :sparkles: feat: 로그인시 응답코드 수정 및 gpt api 기능 구현 로그인 시 유저정보 함께 응답, ggpt api 기능 구현 답변 json 형식 지정 및 길이 제한

--- backend/src/main/java/com/zzol/sizzang/global/login/LoginSuccessHandler.java
+++ backend/src/main/java/com/zzol/sizzang/global/login/LoginSuccessHandler.java
@@ -1,6 +1,9 @@
 package com.zzol.sizzang.global.login;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zzol.sizzang.global.jwt.JwtService;
+import com.zzol.sizzang.user.entity.User;
 import com.zzol.sizzang.user.repository.UserRepository;
 import com.zzol.sizzang.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,8 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,7 +30,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) {
+                                        Authentication authentication) throws IOException {
 
         String userId = extractUsername(authentication); // 인증 정보에서 userId 추출
         String accessToken = jwtService.createAccessToken(userId); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
@@ -33,6 +38,17 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
         // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Optional<User> findUser = userRepository.findByUserId(userId);
+        if (findUser.isPresent()) {
+            User loginUser = findUser.get();
+            String result = objectMapper.writeValueAsString(loginUser);//class를 파싱하여 json 형식 string으로 변환
+            response.getWriter().write(result);
+        }
 
         userRepository.findByUserId(userId)
                 .ifPresent(user -> {
