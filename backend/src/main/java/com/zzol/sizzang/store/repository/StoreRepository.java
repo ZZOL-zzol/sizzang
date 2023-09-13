12b2f2bf91053cf45eb7262433c03b6bb5af930a
정민희 <mong2072@naver.com>
2023-09-13 23:41:24 +0900

    :sparkles: feat: 리뷰등록시 점포 평점 업데이트

--- backend/src/main/java/com/zzol/sizzang/store/repository/StoreRepository.java
+++ backend/src/main/java/com/zzol/sizzang/store/repository/StoreRepository.java
@@ -4,6 +4,7 @@ import com.zzol.sizzang.store.dto.response.StoreFindRes;
 import com.zzol.sizzang.store.entity.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,7 @@ import java.util.Optional;
 @Repository
 public interface StoreRepository extends JpaRepository<StoreEntity, Long>, QStoreRepository {
     List<StoreFindRes> findByMarketEntity_MkCode(int mkCode);
+
+    Optional<StoreEntity> findByStCode(long stCode);
 
 }
