da1a3052caaf8a5f9d9a2d0b1873da705d34fe66
김민식 <sakmsik98@naver.com>
2023-09-14 10:54:46 +0900

    :sparkles: feat: ë regist Product fix

--- backend/src/main/java/com/zzol/sizzang/store/repository/StoreRepository.java
+++ backend/src/main/java/com/zzol/sizzang/store/repository/StoreRepository.java
@@ -1,10 +1,7 @@
 package com.zzol.sizzang.store.repository;
 
-import com.zzol.sizzang.store.dto.response.StoreFindRes;
 import com.zzol.sizzang.store.entity.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface StoreRepository extends JpaRepository<StoreEntity, Long>, QStoreRepository {
 
-    List<StoreFindRes> findByMarketEntity_MkCode(int mkCode);
+    List<StoreEntity> findByMarketEntity_MkCode(int mkCode);
     Optional<StoreEntity> findByStCode(long stCode);
 
 
