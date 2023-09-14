536aec05865ec09ce173ba780c1dde6065e1ff52
정민희 <mong2072@naver.com>
2023-09-14 15:15:09 +0900

    :sparkles: feat: 시장목록 불러오기연결 및 api수정

---
+++ backend/src/main/java/com/zzol/sizzang/market/dto/request/MarketGetReq.java
@@ -0,0 +1,12 @@
+package com.zzol.sizzang.review.dto.request;
+
+import lombok.Data;
+
+
+@Data
+public class ReviewGetReq {
+
+    private int mkCode;
+    private int stCode;
+    private int reCode;
+}
